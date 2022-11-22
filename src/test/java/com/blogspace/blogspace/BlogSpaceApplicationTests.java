5fa76af7db62eb17e80aa7a097696bc60d240203
David Lara <david.a.lara05@gmail.com>
2022-11-22 12:06:02 -0600

    initilize repo

---
+++ src/test/java/com/blogspace/blogspace/BlogSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogspace.blogspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

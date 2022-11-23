03ec81a184ca21dbf4e7738fd10ab4164768ebbc
David Lara <david.a.lara05@gmail.com>
2022-11-23 13:59:06 -0600

    added getters and setters and a constructor ;

--- src/main/java/com/blogspace/models/Blog.java
+++ src/main/java/com/blogspace/models/Blog.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 @Table (name = "blogs")
 public class Blog {
 
+//    Instances
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -15,4 +17,44 @@ public class Blog {
     private String body;
 
 
-}
+//    Getters and Setters
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+//    Constructors
+
+
+    public Blog() {
+    }
+
+    public Blog(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+
+} // End of Blog

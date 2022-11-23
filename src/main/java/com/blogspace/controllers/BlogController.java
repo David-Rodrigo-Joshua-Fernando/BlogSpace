f9c299e49bc8d092aabd6cec17e00561a9e6a0b6
David Lara <david.a.lara05@gmail.com>
2022-11-23 14:17:42 -0600

    added blog.html

--- src/main/java/com/blogspace/controllers/BlogController.java
+++ src/main/java/com/blogspace/controllers/BlogController.java
@@ -2,14 +2,12 @@ package com.blogspace.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/blog")
+//@RequestMapping("/blog")
 public class BlogController {
 
-    @GetMapping()
+    @GetMapping
     public String homePage(){
         return "index";
     }

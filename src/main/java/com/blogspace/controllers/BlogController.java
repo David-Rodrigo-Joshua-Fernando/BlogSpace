7d3a8aa01ffe99dd98cdb3f36beef99e5927c47d
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-12-02 16:35:49 -0600

    BlogController Update, Added Login HTML, Signup HTML, and Utils.java

--- src/main/java/com/blogspace/controllers/BlogController.java
+++ src/main/java/com/blogspace/controllers/BlogController.java
@@ -1,10 +1,14 @@
 package com.blogspace.controllers;
 
 import com.blogspace.models.Blog;
+import com.blogspace.models.User;
 import com.blogspace.repositories.BlogRepository;
+import com.blogspace.services.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,12 +27,21 @@ public class BlogController {
     public String homePage(Model model){
         List<Blog> allBlogs = blogDao.findAll();
         model.addAttribute("allBlogs", allBlogs);
-        return "index";
+        return "blog";
     }
+
 
     @GetMapping("/create")
     public String showCreateBlog(){
         return "/create";
+    }
+
+    @PostMapping("/create")
+    public String submitCreateForm(@ModelAttribute Blog blog){
+        User user = Utils.currentUser();
+        blog.setUser(user);
+        blogDao.save(blog);
+        return "redirect:/blogs";
     }
 
 
---
+++ src/main/java/com/blogspace/services/Utils.java
@@ -0,0 +1,12 @@
+package com.blogspace.services;
+
+import com.blogspace.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class Utils {
+
+    public static User currentUser(){
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+}

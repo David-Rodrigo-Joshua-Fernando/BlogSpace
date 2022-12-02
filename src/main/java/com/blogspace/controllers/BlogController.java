46185f09bc36ee8ec82efec01e5a6189332e62a0
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-12-02 10:00:54 -0600

    BlogController in Progress, created blogspace_db, adjusted application.properties

--- src/main/java/com/blogspace/controllers/BlogController.java
+++ src/main/java/com/blogspace/controllers/BlogController.java
@@ -1,15 +1,23 @@
 package com.blogspace.controllers;
 
+import com.blogspace.models.Blog;
+import com.blogspace.repositories.BlogRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-//@RequestMapping()
+@RequestMapping("/blogs")
 public class BlogController {
 
-    @GetMapping()
+    private final BlogRepository blogDao;
+
+    public BlogController(BlogRepository blogDao){
+        this.blogDao = blogDao;
+    }
+
+    @GetMapping
     public String homePage(){
         return "index";
     }
